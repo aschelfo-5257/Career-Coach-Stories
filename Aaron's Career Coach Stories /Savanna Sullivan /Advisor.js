5296a7d38a8ad1be9ca1c8509309cd74343e79e2
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-08-12 01:38:13 -0400

    Advisor.js

--- Aaron's Career Coach Stories /Savanna Sullivan /Advisor.js
+++ Aaron's Career Coach Stories /Savanna Sullivan /Advisor.js
@@ -1,5 +1,23 @@
 function advisor(Savanna) {
-  console.log("You are my best advisor" + Savanna + "!");
+  console.log("You are my best advisor " + Savanna + "!");
 }
+advisor();
 
+function skillCore(
+  core1 = "Client Relations",
+  core2 = "Medical Coding",
+  core3 = "Financial Management",
+  core4 = "Marketing Strategy",
+  core5 = "Microsoft Office Suite",
+  core6 = "Data Analytics"
+  ) {
+    console.log(`The social skill maintain ${core1} for products and services.`);
+    console.log(`Manage an develop the ${core2} institution.`);
+    console.log(`The ${core3} was tailored a plan to help customers reduce amount due.`);
+    console.log(`Developing a unified brand voice and message that is ${core4}.`);
+    console.log(`Conducted in-depth support the ${core5} to clear objectives and promotional.`);
+    console.log(`Integers market to provide support comprehensive for ${core6}.`);
+  }
+skillCore();  
+  
 console.log("Savanna Sullivan - Advisor Career Coach");
